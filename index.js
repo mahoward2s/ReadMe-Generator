006ae92f642d20457a681c7ac7b9f7a1e5737476
Sportsguy <mahoward2s@gmail.com>
2021-01-20 13:09:44 -0600

    forgot to add json package first before dependencies, also added coade to index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ inquirer
     {
       type: 'input',
       name: 'Installation Instructions',
-      message: '',
+      message: 'default npm i',
     },
     {
       type: 'input',
@@ -53,24 +53,6 @@ inquirer
   .then((answers) => {
 ​
     console.log(answers)
-    
-    html = (`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Added a link to Bootstrap-->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        <title>Document</title>
-    </head>
-        <body>
-            <h1 class="text-center">Name: ${answers.name}</h1>
-            <p class="text-center">Location: ${answers.location}</p>
-            <p class="text-center">Bio: ${answers.bio}</p>
-            <p class="text-center">LinkedIn: ${answers.LinkedIn}</p>
-            <p class="text-center">GitHub: ${answers.GitHub}</p>
-        </body>
-    </html>`)
 
     readMe = (`# ${answers.Title}  
     
@@ -102,7 +84,7 @@ inquirer
     If you want to contribute to the repository you can at github.com/${answers.GitHUb}
     `)
 ​
-    fs.writeFile('index.html', html, function(err){
+    fs.writeFile('README.md', readMe, function(err){
         if (err) throw err;
         console.log('Saved!');
     })
